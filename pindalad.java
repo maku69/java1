ecabdaff9d5dafd7358a6bfae40b9371552b4943
maku69 <81355250+maku69@users.noreply.github.com>
2023-10-15 00:12:32 +0300

    Add files via upload

---
+++ pindalad.java
@@ -0,0 +1,13 @@
+public class pindalad {
+    public static double ruupkylik(int pikkus, int laius) {
+        return pikkus * laius;
+    }
+
+    public static double ristkylik(int pikkus, int laius) {
+        return pikkus * laius;
+    }
+
+    public static double kolmnurk(int alus, int korgus) {
+        return 0.5 * alus * korgus;
+    }
+}
